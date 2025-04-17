7d52f30793e33f2128713cad94f6d8c281975f5f
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-04-17 14:06:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstWord(s) {
-  // your code here
+	let ans='';
+	for(let i=0;i<s.length;i++){
+		if(s[i]!=' '){
+			ans+=s[i];
+		}
+		if(ans.length>0 && s[i]==' ') break;
+	}
+	return ans;
 }
 
 // Do not change the code below
-
+   
 const s = prompt("Enter String:");
 alert(firstWord(s));
